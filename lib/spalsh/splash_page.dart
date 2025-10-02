16d0153528520a509d2b4e9ef789a963b2dd8ed3
akanshujamwal <a.jamwal132@gmail.com>
2025-10-02 22:26:17 +0530

    Code clean

--- lib/spalsh/splash_page.dart
+++ lib/spalsh/splash_page.dart
@@ -1,32 +1,5 @@
-// lib/auth/splash_screen.dart
+
 import 'package:flutter/material.dart';
-
-// class SplashScreen extends StatelessWidget {
-//   const SplashScreen({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return const Scaffold(
-//       backgroundColor: Colors.black,
-//       body: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//             // You can replace this with your app's logo
-//             CircularProgressIndicator(
-//               valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
-//             ),
-//             SizedBox(height: 20),
-//             Text(
-//               'Loading...',
-//               style: TextStyle(color: Colors.white, fontSize: 18),
-//             ),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
 
 
 class SplashScreen extends StatelessWidget {
