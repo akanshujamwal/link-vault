0181d1276a79bac304573c2ddd5a0d8c72e1e072
akanshujamwal <a.jamwal132@gmail.com>
2025-10-11 18:27:02 +0530

    removed unused file

--- lib/scanner/page/generate_qr_code_page.dart
+++ lib/scanner/page/generate_qr_code_page.dart
@@ -1,31 +1,31 @@
-import 'package:flutter/material.dart';
-import 'package:qr_flutter/qr_flutter.dart';
+// import 'package:flutter/material.dart';
+// import 'package:qr_flutter/qr_flutter.dart';
 
-class GenerateQrCodePage extends StatefulWidget {
-  const GenerateQrCodePage({super.key});
+// class GenerateQrCodePage extends StatefulWidget {
+//   const GenerateQrCodePage({super.key});
 
-  @override
-  State<GenerateQrCodePage> createState() => _GenerateQrCodePageState();
-}
+//   @override
+//   State<GenerateQrCodePage> createState() => _GenerateQrCodePageState();
+// }
 
-class _GenerateQrCodePageState extends State<GenerateQrCodePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            QrImageView(
-              data: "Hello, World!", // The string you want to encode
-              size: 200,
-              version: QrVersions.auto,
-            ),
-            SizedBox(height: 20),
-            Text("Scan this QR code"),
-          ],
-        ),
-      ),
-    );
-  }
-}
+// class _GenerateQrCodePageState extends State<GenerateQrCodePage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             QrImageView(
+//               data: "", // The string you want to encode
+//               size: 200,
+//               version: QrVersions.auto,
+//             ),
+//             SizedBox(height: 20),
+//             Text("Scan this QR code"),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
